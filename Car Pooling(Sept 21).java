be83b6791c46308be9b57695d7dfd77295c3781a
Shashank Jha <66532738+kumarshashank571@users.noreply.github.com>
2020-09-23 00:27:11 +0530

    Create Car Pooling(Sept 21).java

---
+++ Car Pooling(Sept 21).java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean carPooling(int[][] trips, int capacity) {
+        int[] timestamp = new int[1001];
+        for (int[] trip : trips) {
+            timestamp[trip[1]] += trip[0];
+            timestamp[trip[2]] -= trip[0];
+        }
+        int ued_capacity = 0;
+        for (int number : timestamp) {
+            ued_capacity += number;
+            if (ued_capacity > capacity) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
